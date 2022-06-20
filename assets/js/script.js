06c66568ab0a0c7d8e6ab8dd9a0b690bda52e844
Busatron <bussea38@gmail.com>
2022-06-19 19:06:32 -0500

    event listner added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
-var buttonE1 = document.querySelector("#save-task");
+var formE1 = document.querySelector("#task-form");
 var tasksToDOE1 = document.querySelector("#task-to-do")
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+
+    event.preventDefault();
+
     var taskItemE1 = document.createElement("li");
     listItemE1.classname = "task-item";
     listItemE1,textContent = "This is a new task.";
     tasksToDOE1.appendChild(listItemE1);
 };
 
-buttonE1.addEventListener("click", createTaskHandler);
+formE1.addEventListener("submit", createTaskHandler);
 
